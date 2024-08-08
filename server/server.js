eed63c17bf29a2b1389312884a466b07e60f4029
Emmanuel-Castillo <emmanuel.2001alonso.ec@gmail.com>
2024-08-08 14:19:19 -0700

    Fixed styling for game

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
     origin:
-      "http://localhost:3000" || "https://durak-a6f8ab3ff9e1.herokuapp.com", // Allow requests from this origin
+      "https://durak-a6f8ab3ff9e1.herokuapp.com" || "http://localhost:3000", // Allow requests from this origin
     methods: ["GET", "POST"],
   },
 });
@@ -508,6 +508,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(4000 || process.env.PORT, () => {
+server.listen(process.env.PORT || 4000, () => {
   console.log("listening on server");
 });
