6af3d9881f7b13bced1e3b8695a0b73c731d1705
Emmanuel-Castillo <emmanuel.2001alonso.ec@gmail.com>
2024-08-08 13:48:14 -0700

    changed order of URLS

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -17,7 +17,7 @@ export const SocketProvider = ({ children }) => {
   useEffect(() => {
     
     // Create a socket connection to the server
-    const serverURL = 'http://localhost:4000' || process.env.REACT_APP_SERVER_URL;
+    const serverURL = process.env.REACT_APP_SERVER_URL || 'http://localhost:4000'  ;
     const newSocket = io(serverURL);
     setSocket(newSocket);
 
